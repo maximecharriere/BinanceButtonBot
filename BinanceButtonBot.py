622a7d9e49e970eb835cc3a3a92e83f1d6b9a750
maximecharriere <51918753+maximecharriere@users.noreply.github.com>
2022-03-25 14:30:36 +0100

    1st version fonctionelle

--- BinanceButtonBot.py
+++ BinanceButtonBot.py
@@ -1,24 +1,34 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
-import pandas as pd
 from time import time, sleep
+import csv
 
 driver = webdriver.Chrome("C:/dev/chromedriver_win32/chromedriver.exe")
 driver.get("https://www.binance.com/en/activity/bitcoin-button-game")
 
-start = time()
-counter = []
-while True:
-    sleep(1.0 - time() % 60)
-    content = driver.page_source
-    soup = BeautifulSoup(content, features="html.parser")
-    
-    matches =  soup.findAll('div', attrs={'class':'css-w39bvu'})
-    counter.append(float(matches[0].text+matches[1].text+'.'+matches[2].text+matches[3].text))
+countdown = []
+start_m = 0
+with open('BinanceButtonStats.csv', 'a', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-    if (time() >= start+60.0):
-        start = time()
-        matches =  soup.findAll('div', attrs={'class':'css-th68ec'})
-        totalParticipants = int(matches[1].text.replace(",",""))
-    
-    
+    while True:
+        start_s = time()
+        content = driver.page_source
+        soup = BeautifulSoup(content, features="html.parser")
+        
+        matches =  soup.findAll('div', attrs={'class':'css-w39bvu'})
+        countdown.append(float(matches[0].text+matches[1].text+'.'+matches[2].text+matches[3].text))
+        if (start_s >= start_m+60.0):
+            start_m = start_s
+
+            countdown_min = min(countdown)
+            countdown.clear()
+            matches =  soup.findAll('div', attrs={'class':'css-th68ec'})
+            totalParticipants = int(matches[1].text.replace(",",""))
+            csvwriter.writerow([start_m,countdown_min,totalParticipants])
+            print(f"Row writed: {start_m},{countdown_min},{totalParticipants}")
+
+        to_sleep = 1.0+start_s-time()
+        sleep(to_sleep) if to_sleep>=0 else print("PROCESS TIME > 1s !!")
+        
+        
