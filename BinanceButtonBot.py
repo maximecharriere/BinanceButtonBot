1ae44021177e3ca98321c5dde208212c971b5a66
maximecharriere <51918753+maximecharriere@users.noreply.github.com>
2022-03-29 23:24:46 +0200

    fonctional with mysql

--- BinanceButtonBot.py
+++ BinanceButtonBot.py
@@ -4,22 +4,25 @@ from bs4 import BeautifulSoup
 from time import time, sleep
 import csv
 import mysql.connector
-import sys
 
-# try:
-#     mydb = mysql.connector.connect(
-#     host="maximecharriere.ch",
-#     user="root",
-#     password="##########",
-#     database="crypto"
-#     )
-# except mysql.connector.Error as e:
-#     print(f"Error connecting to MariaDB Platform: {e}")
-#     sys.exit(1)
+mysql_insert_query = """
+INSERT INTO `binance_btn_stats` (`timestamp`, `participants_tot`, `countdown_min`) 
+VALUES 
+(FROM_UNIXTIME(%s), %s, %s);
+"""
 
-# mycursor = mydb.cursor()
+try:
+    mydb = mysql.connector.connect(
+        host="",
+        user="",
+        password="",
+        database="")
+    mycursor = mydb.cursor()
 
-# CHROME_PATH = "C:/dev/chromedriver_win32/chromedriver.exe"
+except mysql.connector.Error as e:
+    print(f"Failed to connect to database: {e}")
+    exit()
+
 CHROMEDRIVER_PATH = "C:/dev/chromedriver_win32/chromedriver.exe"
 WINDOW_SIZE = "1920,1080"
 URL = "https://www.binance.com/en/activity/bitcoin-button-game"
@@ -28,7 +31,6 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
-# chrome_options.binary_location = CHROME_PATH
 
 driver = webdriver.Chrome(
     executable_path=CHROMEDRIVER_PATH,
@@ -55,15 +57,15 @@ with open('BinanceButtonStats.csv', 'a', newline='') as csvfile:
             countdown_min = min(countdown)
             countdown.clear()
             matches =  soup.findAll('div', attrs={'class':'css-th68ec'})
-            totalParticipants = int(matches[1].text.replace(",",""))
-            csvwriter.writerow([start_m,countdown_min,totalParticipants])
-            print(f"Row writed: {start_m},{countdown_min},{totalParticipants}")
-            # try:
-            #     mycursor.execute(
-            #     "INSERT INTO binance_btn_stats (timestamp,participants_tot,countdown_min) VALUES (?, ?, ?)", 
-            #     (start_m,totalParticipants,countdown_min))
-            # except mysql.connector.Error as e:
-            #     print(f"Error adding data to MariaDB Platform: {e}")
+            participants = int(matches[1].text.replace(",",""))
+            csvwriter.writerow([start_m,countdown_min,participants])
+            print(f"Row writed: {start_m},{countdown_min},{participants}")
+            try:
+                mycursor.execute(mysql_insert_query, (start_m,participants,countdown_min))
+                mydb.commit()
+            except mysql.connector.Error as e:
+                print(f"Failed to insert to database: {e}")
+                exit()
 
         to_sleep = 1.0+start_s-time()
         sleep(to_sleep) if to_sleep>=0 else print("PROCESS TIME > 1s !!")
