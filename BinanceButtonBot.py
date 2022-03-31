4ac86c01949d3b544500a8035b3cebdc997e6012
maximecharriere <51918753+maximecharriere@users.noreply.github.com>
2022-03-31 22:04:27 +0200

    Use Remote WebDriver

--- BinanceButtonBot.py
+++ BinanceButtonBot.py
@@ -1,17 +1,14 @@
-from typing import final
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-
 from bs4 import BeautifulSoup
 from time import time, sleep
 import mysql.connector
-import json 
+import json
 import requests
 
-CHROMEDRIVER_PATH = "C:/dev/chromedriver_win32/chromedriver.exe"
 URL = "https://www.binance.com/en/activity/bitcoin-button-game"
 
 MYSQL_QUERY = """
@@ -20,12 +17,15 @@ VALUES
 (%s, %s, %s);
 """
 
-def main ():
+REMOTE_WEBDRIVER = True
+
+
+def main():
     try:
 
         ### CONFIG ###
         try:
-            r =requests.get(URL)
+            r = requests.get(URL)
             if (r.status_code != 200):
                 raise ReferenceError(f"URL '{URL}' isn't accessible")
         except ReferenceError as e:
@@ -41,10 +41,10 @@ def main ():
 
         try:
             mydb = mysql.connector.connect(
-                host     = login["host"],
-                user     = login["user"],
-                password = login["password"],
-                database = login["database"])
+                host=login["host"],
+                user=login["user"],
+                password=login["password"],
+                database=login["database"])
             mycursor = mydb.cursor()
         except mysql.connector.Error as e:
             print(f"{type(e).__name__} : {e}")
@@ -54,11 +54,13 @@ def main ():
             chrome_options = Options()
             chrome_options.add_argument("--headless")
             chrome_options.add_argument('--disable-gpu')
-            s=Service(ChromeDriverManager().install())
-            driver = webdriver.Chrome(
-                service=s,
-                options=chrome_options)
-            # driver = webdriver.Remote("http://localhost:4444/wd/hub", options=chrome_options)
+            if REMOTE_WEBDRIVER:
+                driver = webdriver.Remote(
+                    "http://localhost:4444/wd/hub", options=chrome_options)
+            else:
+                driver = webdriver.Chrome(
+                    service=Service(ChromeDriverManager().install()),
+                    options=chrome_options)
         except WebDriverException as e:
             print(f"{type(e).__name__} : {e}")
             return 1
@@ -72,27 +74,30 @@ def main ():
             start_s = time()
             content = driver.page_source
             soup = BeautifulSoup(content, features="html.parser")
-            
-            matches =  soup.findAll('div', attrs={'class':'css-w39bvu'})
-            countdown.append(float(matches[0].text+matches[1].text+'.'+matches[2].text+matches[3].text))
+
+            matches = soup.findAll('div', attrs={'class': 'css-w39bvu'})
+            countdown.append(
+                float(matches[0].text+matches[1].text+'.'+matches[2].text+matches[3].text))
             if (start_s >= start_m+60.0):
                 start_m = start_s
 
                 countdown_min = min(countdown)
                 countdown.clear()
-                matches =  soup.findAll('div', attrs={'class':'css-th68ec'})
-                participants = int(matches[1].text.replace(",",""))
+                matches = soup.findAll('div', attrs={'class': 'css-th68ec'})
+                participants = int(matches[1].text.replace(",", ""))
                 try:
-                    mycursor.execute(MYSQL_QUERY, (start_m,participants,countdown_min))
+                    mycursor.execute(
+                        MYSQL_QUERY, (start_m, participants, countdown_min))
                     mydb.commit()
                 except mysql.connector.Error as e:
                     print(f"{type(e).__name__} : {e}")
                     return 2
                 else:
-                    print(f"Row writed: {start_m}, {participants}, {countdown_min}")
+                    print(
+                        f"Row writed: {start_m}, {participants}, {countdown_min}")
 
             to_sleep = 1.0+start_s-time()
-            sleep(to_sleep) if to_sleep>=0 else print("PROCESS TIME > 1s !!")
+            sleep(to_sleep) if to_sleep >= 0 else print("PROCESS TIME > 1s !!")
 
     finally:
         if mydb is not None:
@@ -103,8 +108,5 @@ def main ():
             driver.quit()
 
 
-
 if __name__ == "__main__":
     exit(main())
-
-
